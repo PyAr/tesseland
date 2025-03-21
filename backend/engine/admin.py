969b9467cd61f579cc87cf5cf1135cfe9550ebe1
matuu <matu.varela@gmail.com>
2025-03-21 14:58:32 -0300

    add models, admins, assets manager

--- backend/engine/admin.py
+++ backend/engine/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from engine.models import Game, Player, GamePlayer
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status')
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class GamePlayerAdmin(admin.ModelAdmin):
+    list_display = ('game', 'player', 'figure')
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(GamePlayer, GamePlayerAdmin)
--- backend/engine/models.py
+++ backend/engine/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class GameStatus(models.TextChoices):
+    WAITING = 'WAITING'
+    PLAYING = 'PLAYING'
+    FINISHED = 'FINISHED'
+
+
+class Player(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=10, unique=True)
+
+
+class Game(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    picture = models.ImageField(upload_to='games')
+    status = models.CharField(max_length=10, choices=GameStatus.choices)
+
+
+class GamePlayer(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    figure = models.ImageField(upload_to='figures')
